a087795728cc26449de2444f262f0c24e5a83065
Aravind Balaji <aravind.balaji@live.com>
2017-02-19 23:21:20 +0530

    Update watty_ebook.py

--- watty_ebook.py
+++ watty_ebook.py
@@ -1,4 +1,4 @@
-#Pytho script to web-scrape Wattpad Books and prepare a HTML file of the book
+#Python script to web-scrape Wattpad Books and prepare a HTML file of the book
 #The HTML file itself is the entire eBook
 #It can later be converted to EPUB or MOBI formats using eBook Generators such as KindleGen
 
